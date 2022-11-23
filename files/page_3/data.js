1cb2a7a456c1135ed0d218b3a715ec51a478a204
DankataBSL <118892899+DankataBSL@users.noreply.github.com>
2022-11-23 12:54:22 +0200

    Add files via upload

---
+++ files/page_3/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,[i],j,_(k,l,m,n,o,p,q,_(r,s,t,u,v,_(w,x,y,z),A,null,B,u,C,u,D,E,F,null,G,H,I,J,K,L,M,H),N,_(),O,_(),P,_(Q,[])),R,_(),S,_());}; 
+var b="url",c="page_3.html",d="generationDate",e=new Date(1669198196354.34),f="isCanvasEnabled",g=false,h="variables",i="OnLoadVariable",j="page",k="packageId",l="d1d3218e0e4a4ec7b3e91c54f37b9856",m="type",n="Axure:Page",o="name",p="Page 3",q="style",r="baseStyle",s="627587b6038d43cca051c114ac41ad32",t="pageAlignment",u="near",v="fill",w="fillType",x="solid",y="color",z=0xFFFFFFFF,A="image",B="imageHorizontalAlignment",C="imageVerticalAlignment",D="imageRepeat",E="auto",F="favicon",G="sketchFactor",H="0",I="colorStyle",J="appliedColor",K="fontName",L="Applied Font",M="borderWidth",N="adaptiveStyles",O="interactionMap",P="diagram",Q="objects",R="masters",S="objectPaths";
+return _creator();
+})());
